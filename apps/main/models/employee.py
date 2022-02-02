e78087d89609442d48dec1030271cecd042bc74b
major-hub <mrsol1jonovi4@gmail.com>
2022-02-02 10:26:25 +0500

    added some fields to Employee model

--- apps/main/models/employee.py
+++ apps/main/models/employee.py
@@ -9,6 +9,12 @@ class Employee(BaseModel):
     full_name = models.CharField(max_length=255)
     image = models.ForeignKey(File, on_delete=models.SET_NULL, null=True)
     about = models.TextField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    time_to_apply = models.CharField(max_length=150, blank=True)
+    instagram = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
+    telegram = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
 
     class Meta:
         verbose_name = _('Employee')
